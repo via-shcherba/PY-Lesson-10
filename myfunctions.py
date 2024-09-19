0e90beda18df856aa91fa0b6fe63952efeb7e9bb
via.shcherba <47893846+via-shcherba@users.noreply.github.com>
2024-09-19 18:58:02 +0200

    changes

--- myfunctions.py
+++ myfunctions.py
@@ -12,7 +12,7 @@ def simple_separator():
     Функция создает красивый резделитель из 10-и звездочек (**********)
     :return: **********
     """
-    pass
+    return '*' * 10
 
 
 print(simple_separator() == '**********')  # True
@@ -24,7 +24,7 @@ def long_separator(count):
     :param count: количество звездочек
     :return: строка разделитель, примеры использования ниже
     """
-    pass
+    return '*' * count
 
 
 print(long_separator(3) == '***')  # True
@@ -38,7 +38,7 @@ def separator(simbol, count):
     :param count: количество повторений
     :return: строка разделитель примеры использования ниже
     """
-    pass
+    return simbol * count
 
 
 print(separator('-', 10) == '----------')  # True
@@ -55,7 +55,9 @@ def hello_world():
     ##########
     :return: None
     """
-    pass
+    print(simple_separator())
+    print("\nHello World!\n")
+    print(separator('#', 10))
 
 
 '''
@@ -79,7 +81,9 @@ def hello_who(who='World'):
     :param who: кого мы приветствуем, по умолчанию World
     :return: None
     """
-    pass
+    print(simple_separator())
+    print(f"\nHello {who}!\n")
+    print(separator('#', 10))
 
 
 '''
@@ -115,7 +119,7 @@ def pow_many(power, *args):
     :param args: любое количество цифр
     :return: результат вычисления # True -> (1 + 2)**1
     """
-    pass
+    return sum(args) ** power
 
 
 print(pow_many(1, 1, 2) == 3)  # True -> (1 + 2)**1 == 3
@@ -133,7 +137,8 @@ def print_key_val(**kwargs):
     :param kwargs: любое количество именованных параметров
     :return: None
     """
-    pass
+    for key, value in kwargs.items():
+        print(f"{key} --> {value}")
 
 
 """
@@ -158,7 +163,7 @@ def my_filter(iterable, function):
     :param function: функция фильтрации
     :return: новая отфильтрованная последовательность
     """
-    pass
+    return [item for item in iterable if function(item)]
 
 
 print(my_filter([1, 2, 3, 4, 5], lambda x: x > 3) == [4, 5])  # True
